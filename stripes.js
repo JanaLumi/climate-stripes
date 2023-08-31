80fa9745857aa87b19eb733a0ae5f8a776024d25
GreySkySpin <42599782+GreySkySpin@users.noreply.github.com>
2023-08-31 16:10:39 +0300

    Update stripes.js

--- stripes.js
+++ stripes.js
@@ -14,10 +14,8 @@ function processData(datum) {
 	return dataItem;
 }
 
-//var globalData;
 function graph(data, id) {
 	console.log(id, data);
-//	globalData = data;
 	let colours = ["#08306b" , "#08519c", "#2171b5", "#4292c6", "#6baed6", "#9ecae1", "#c6dbef", "#deebf7", "#f7fbff", "#fff5eb", "#fee6ce", "#fdd0a2", "#fdae6b", "#fd8d3c", "#f16913", "#d94801", "#a63603", "#7f2704"];
 	let stripeWidth = 4;
 	let stripeHeight = 300;
@@ -30,13 +28,23 @@ function graph(data, id) {
 	let svg = d3.select(id).append("svg")
 					 .attr("width", data.length * stripeWidth)
 					 .attr("height", stripeHeight);
+					 
+	let tip = d3.tip()
+        .attr('class', 'd3-tip')
+        .html(function (d) {
+            return `<strong>Year:</strong> ${d.year}<br><strong>Avg:</strong> ${d.avg}`;
+        });
+	
+	svg.call(tip);
+	
 	let stripes = svg.selectAll("rect")
-						   .data(data).enter()
-						   .append("rect")
-						   .attr("width", stripeWidth)
-						   .attr("height", stripeHeight)
-						   .attr("x", (d, i) => i * stripeWidth)
-						   .attr("y", 0)
-						   .style("fill", (d, i) => colours[Math.round(linearScaleForData(d.avg))])
-						   .on('mouseover', (d, i) => console.log(d.year));
+							.data(data).enter()
+							.append("rect")
+							.attr("width", stripeWidth)
+							.attr("height", stripeHeight)
+							.attr("x", (d, i) => i * stripeWidth)
+							.attr("y", 0)
+							.style("fill", (d, i) => colours[Math.round(linearScaleForData(d.avg))])
+							.on('mouseover', tip.show) // Show tooltip on mouseover
+							.on('mouseout', tip.hide); // Hide tooltip on mouseout
 }
